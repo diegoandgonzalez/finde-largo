0379d035dc621a8d16cb38e39e210b121d035780
Diego <diego.gonzalez@albany.company>
2023-05-19 18:56:42 -0300

    add fetch revalidation

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
+import { TIME_TO_REVALIDATE } from "./utils/constants";
+
 export default async function Home() {
-  const res = await fetch('http://localhost:3000/api/days-until-next-long-weekend');
+  const res = await fetch('http://localhost:3000/api/days-until-next-long-weekend', { next: { revalidate: TIME_TO_REVALIDATE } });
   const data = res.status === 200 ? await res.json() : null;
 
   return (
