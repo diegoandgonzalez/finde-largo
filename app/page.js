28179c4a1f6378b178b59f6ff86b635ae0333900
Diego <diego.gonzalez@albany.company>
2023-05-19 18:54:04 -0300

    init main page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,21 @@
-export default function Home() {
+export default async function Home() {
+  const res = await fetch('http://localhost:3000/api/days-until-next-long-weekend');
+  const data = res.status === 200 ? await res.json() : null;
+
   return (
     <main>
+      {
+        data ?
+          <>
+            <h1>{data.daysUntilHoliday}</h1>
+            <p>{data.date}</p>
+            <p>{data.description}</p>
+          </>
+          :
+          <h1>
+            Error
+          </h1>
+      }
     </main>
   )
 }
