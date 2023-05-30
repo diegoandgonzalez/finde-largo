c1c1a13e97677f53250746fc6bbac8aa83388bc0
Diego <diego.gonzalez@albany.company>
2023-05-30 15:08:47 -0300

    add main page styling

--- app/page.js
+++ app/page.js
@@ -12,24 +12,26 @@ import {
 const getDaysUntilText = (amountOfDays) => {
   if (amountOfDays <= 0) return <>Ya arrancó el <b>finde largo</b> 🥳</>;
   if (amountOfDays <= 7) return <>El <b>finde largo</b> arranca en <b>{amountOfDays}</b> días 🥵</>;
-  return <>Faltan <b>{amountOfDays}</b> días para el <b>finde largo</b>😴</>;
+  return <>Faltan <b>{amountOfDays}</b> días para el <b>finde largo</b> 😴</>;
 }
 
 export default async function Home() {
   const data = await getData(`${API_URL}/api/days-until-next-long-weekend?from=${formatDateToYYYYMMDD(getTodayObject())}`);
 
   return (
-    <main>
+    <main className="w-4/5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
       {
         data ?
           <>
-            <h1>{getDaysUntilText(data.daysUntilLongWeekend)}</h1>
-            <p>{formatDateLongText(data.date)}</p>
-            <p>{data.description}</p>
-            <Link href="/next-long-weekends">Ver próximos findes largos</Link>
+            <h1 className="text-5xl font-semibold">{getDaysUntilText(data.daysUntilLongWeekend)}</h1>
+            <div className="m-6">
+              <p className="text-4xl">{formatDateLongText(data.date)}:</p>
+              <p className="text-3xl mt-2 italic">{data.description}</p>
+            </div>
+            <Link className="text-3xl underline" href="/next-long-weekends">Ver próximos findes largos</Link>
           </>
           :
-          <h1>Algo malió sal 🧂</h1>
+          <h1 className="text-6xl">Algo <b>malió sal</b> 🧂</h1>
       }
     </main>
   )
