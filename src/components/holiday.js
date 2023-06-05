165a80a4296d6de9a873dc57dc93d3985094db74
Diego <diego.gonzalez@albany.company>
2023-06-05 12:07:46 -0300

    styling

--- src/components/holiday.js
+++ src/components/holiday.js
@@ -7,7 +7,7 @@ const Holiday = ({ date, description, small }) => {
     return (
         <>
             <p className={small ? "text-xl" : "text-2xl md:text-4xl"}>{formatDateLongText(date)}:</p>
-            <p className={small ? "text-lg italic" : "text-xl md:text-3xl mt-2 italic"}>{description}</p>
+            <p className={small ? "text-lg italic font-medium" : "text-xl md:text-3xl mt-2 italic"}>{description}</p>
         </>
     )
 }
